64aa09cb8558c4780cd36e897bcb2425ed81d01e
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-11-13 14:08:55 +0100

    Singleton

---
+++ Singleton/Configuration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Configuration
+    {
+        private static Configuration configuration = null;
+        private static object obj= new object();
+        public string Property { get; set; }
+        public int IntProperty { get; set; }
+
+        private Configuration()
+        {
+        }
+
+        public static Configuration GetConfiguration()
+        {
+            lock (obj)
+            {
+                if (configuration == null)
+                {
+                    configuration = new Configuration();
+                }
+            }
+
+            return configuration;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿using Singleton;
+
+//var cfg = new Configuration();
+
+var cfg = Configuration.GetConfiguration();
+var cfg2 = Configuration.GetConfiguration();
+
+if (ReferenceEquals(cfg, cfg2))
+{
+    Console.WriteLine("Configuration is a singleton");
+}
